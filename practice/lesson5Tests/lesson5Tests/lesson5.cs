01cd80e39c7a4f023031e12110576d286ecf820f
InspectorTusovka <burashnikov.sergei@gmail.com>
2020-12-17 16:56:20 +0300

    Added lesson-5 tasks

---
+++ practice/lesson5Tests/lesson5Tests/lesson5.cs
@@ -0,0 +1,47 @@
+﻿namespace lesson5Tests
+{
+    class lesson5
+    {
+
+        static void Main()
+        {
+            //TASK_1 Demonstration
+
+            //Console.WriteLine(stringLibrary.CheckLogin_NoRegex());
+            //stringLibrary.CheckLogin_Regex();
+
+            //TASK_2 Demonstration
+
+            //GET_N_LETTERS_WORD METHOD DEMONSTRATION
+            //Message.Get_N_LettersWord("это слово состоит не из шести букв, даже не из четырех, а из пяти",5);
+
+            //REMOVE_WORDS METHOD DEMONSTRATION
+            //Console.WriteLine(Message.RemoveWords($"Здесь нужно убрать слова, оканчивающиеся на мягкий знак - \'ь\'",'ь'));
+
+            //TEXT_LONGEST_WORDS METHOD DEMONSTRATION
+            //Console.WriteLine(Message.TextLongestWords($"Самое длинное слово этого сообщения - это \'сообщение\'"));
+
+
+            //GET_WORD_FREQUENCES METHOD DEMONSTRATION
+            //string[] str = new string[] { "привет", "ноль", "отказ", "мышь", "ваза", "мой", "ливень" };
+            //string strSub = "приветствие, приветы, нолевой, ноль: отказа; Мышки, мышиный, мышь, моего, мойка, лив, мои, ноли";
+            //Dictionary<string, int> res = Message.GetWordFreqeuences(str,strSub);
+            //foreach (string word in res.Keys.ToList())
+            //{
+            //    Console.WriteLine($"Слово: \"{word}\"  встречается в тексте {res[word]} раз");
+            //}
+
+
+            //TASK_3 Demonstration
+            //CHECK_TRANSPOSITION METHOD DEMONSTRATION
+            //Console.WriteLine(stringLibrary.CheckTranspositon("любая строка","рокаст аябюл"));
+
+
+            //TASK_4 Demonstration
+            //GET_WORST_STUDENT METHOD DEMONSTRATION
+            //Exam.GetWorstStudents();
+            #region 
+            #endregion
+        }
+    }
+}
